5ee3cc8eae5c631acb5bceb88c64332669cfb683
t179a <67852630+t179a@users.noreply.github.com>
2020-11-22 22:13:51 +0900

    Create ex12.cpp

---
+++ chapter1/ex12.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+
+  // ここにプログラムを追記
+  
+  int answer = 1;
+  for (int i = 0; i < S.size(); i++){
+    if(S.at(i) == '+'){
+      answer++;
+    }
+    if(S.at(i) == '-'){
+      answer--;
+    }
+  }
+  cout << answer << endl;
+}
